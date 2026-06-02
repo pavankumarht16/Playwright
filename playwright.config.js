c1cb41804eece145698580f8998a6329be8e5aba
rxpavan16 <pavankumarht44>
2026-06-02 13:52:36 +0530

    Fixing playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,20 @@ import {defineConfig} from '@playwright/test'
 export default defineConfig({
     use:{
         baseURL:'http://localhost:5173/',
-        headless:false
+        headless:true
+    },
+     webServer: [
+    {
+      command: 'npm run dev --prefix api',
+      port: 5000,
+      timeout: 120000,
+      reuseExistingServer: !process.env.CI
+    },
+    {
+      command: 'npm run dev --prefix frontend',
+      port: 5173,
+      timeout: 120000,
+      reuseExistingServer: !process.env.CI
     }
+  ]
 })
